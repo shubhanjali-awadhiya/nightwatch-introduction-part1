85427feeadda0aed0e87331e97fdab4712008fdf
Shubhanjali awadhiya <shubhanjaliawadhiya@Shubhanjalis-MacBook-Air.local>
2018-01-24 09:15:09 -0700

    3.2 stretch

---
+++ nightwatch/step4/supporting/data.js
@@ -0,0 +1,14 @@
+module.exports = {
+    '2+2=4' : {
+        button_click_1 : '2',
+        button_click_2 : '+',
+        button_click_3 : '2',
+        button_click_4 : '=',
+        result_1 : '0',
+        result_2 : '4'
+        // add test : {
+          //  button: ['2' '+' '2' '=']
+        // result: 4 
+        }
+    }
+}
--- nightwatch/step4/tests/tests.js
+++ nightwatch/step4/tests/tests.js
@@ -1,6 +1,6 @@
 const selectors = require('../supporting/selectors')
 const functions = require('../supporting/functions')
-
+const data = require('../supporting/data')
 module.exports = {
     beforeEach : browser => {
         browser.url('http://localhost:3000')
@@ -13,16 +13,18 @@ module.exports = {
         //I click all the appropriate buttons and check the display for the appropriate results, per the steps of my test case
         browser
             .click(selectors['2'])
-            .expect.element(selectors['result']).text.to.equal('2')
+            .expect.element(selectors['result']).text.to.equal(data['2+2=4'].button_click_1)
         browser
             .click(selectors['+'])
-            .expect.element(selectors['result']).text.to.equal('0')
+            .expect.element(selectors['result']).text.to.equal(data['2+2=4'].result_1)
         browser
             .click(selectors['2'])
-            .expect.element(selectors['result']).text.to.equal('2')
+            .expect.element(selectors['result']).text.to.equal(data['2+2=4'].button_click_3)
         browser
             .click(selectors['='])
-            .expect.element(selectors['result']).text.to.equal('4')
+            .expect.element(selectors['result']).text.to.equal(data['2+2=4'].result_2)
+            // functions.buttonClicker(browser, data.addtest.button[0]
+        // here we are using array
     },
     '32.1*2=64.2' : browser => {
         functions.buttonClicker(browser, '3')
